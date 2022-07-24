3f8881eddaa38c6d67fff21e6cd59b74ba75eeaa
Pagante <55301195+Pagante@users.noreply.github.com>
2022-07-24 14:10:01 +0100

    whitenoise

--- Ecodig/settings.py
+++ Ecodig/settings.py
@@ -35,12 +35,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'Ecodig.urls'
@@ -113,23 +115,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "/static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR/ "mediaProt"/ "media"
 
 PROTECTED_MEDIA = BASE_DIR/ "mediaProt"/ "protected"
 
-# if DEBUG:
-#     STATIC_ROOT.mkdir(parents=True, exist_ok=True)
-#     MEDIA_ROOT.mkdir(parents=True, exist_ok=True)
-#     PROTECTED_MEDIA.mkdir(parents=True, exist_ok=True)
+if DEBUG:
+    STATIC_ROOT.mkdir(parents=True, exist_ok=True)
+    MEDIA_ROOT.mkdir(parents=True, exist_ok=True)
+    PROTECTED_MEDIA.mkdir(parents=True, exist_ok=True)
     
 
 
