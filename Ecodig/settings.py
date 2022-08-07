bd8f037b195122a2f91f0672bca81ea203469275
Pagante <55301195+Pagante@users.noreply.github.com>
2022-08-07 22:02:57 +0100

    static files

--- Ecodig/settings.py
+++ Ecodig/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paypal.standard.ipn',
+    # 'paypal.standard.ipn',
     'analytics',
     'emails',
     'products',
